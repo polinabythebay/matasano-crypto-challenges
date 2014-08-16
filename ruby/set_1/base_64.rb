3b6a8df3f5b3bc3e352da0fe6288af1b95066a6b
Polina Soshnin <psoshnin@wellesley.edu>
2014-08-16 15:36:28 -0700

    ruby convert hex to base 64

---
+++ ruby/set_1/base_64.rb
@@ -0,0 +1,11 @@
+
+def hex_to_base_64(hex_string)
+	#interpret string as hex
+	hex_enc = [hex_string].pack('H*')
+	#interpret hex as base 64
+	base_64_enc = [hex_enc].pack("m0")
+	return base_64_enc
+end
+
+hex_to_base_64("49276d206b696c6c696e6720796f757220627261696e206c"\
+				"696b65206120706f69736f6e6f7573206d757368726f6f6d")
